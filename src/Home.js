c2c803e88133b820834b7db26d3fe98e8bae5d6e
Sarfeher <132908720+Sarfeher@users.noreply.github.com>
2024-08-21 07:41:29 +0200

    Initial commit

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
+const Home = () => {
+const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
+
+    return (
+        <div className="home">
+            {error && <div>{error}</div>}
+            {isPending && <h1> "Loading ...</h1>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+        </div>
+    );
+}
+
+export default Home;
